361d5f15a99434f1ed90dfb9edc923cb64f47656
Akuete <kuetman@mail.com>
2026-03-19 08:25:52 -0400

    Code updated

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -56,8 +56,27 @@ regd_users.post("/login", (req,res) => {
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    const username = req.user; 
+  
+    if (!review) {
+      return res.status(400).json({ message: "Review content is required" });
+    }
+  
+    const book = books[isbn]; 
+  
+    if (book) {
+      if (!book.reviews) {
+        book.reviews = {};
+      }
+      book.reviews[username] = review; // add/update review by user
+  
+      return res.status(200).json({ message: "Review updated", reviews: book.reviews });
+    } else {
+      return res.status(404).json({ message: "Book not found" });
+    }
 });
 
 module.exports.authenticated = regd_users;
