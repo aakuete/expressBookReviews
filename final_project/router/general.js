8abbbe82c8af6d6e0a8b6f16ac243bfc2bffe4ce
Akuete <kuetman@gmail.com>
2026-04-14 22:26:46 -0400

    Task 11 completed

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -57,18 +57,35 @@ public_users.get('/',function (req, res) {
     });
 });
 
-// Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  const isbn = req.params.isbn;
-  const book = books[isbn];
-  if (book){
-    return res.status(200).json(books[isbn]);
-  } else {
-    return res.status(404).json({ message: "Book not found"});
-  }
 
- });
+// Function to get book details by ISBN using a Promise
+function getBookByISBN(isbn) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        const book = books[isbn];  // if books is an object keyed by ISBN
   
+        if (book) {
+          resolve(book);
+        } else {
+          reject("Book not found");
+        }
+      }, 1000); // simulate async delay
+    });
+  }
+  
+// Get book details based on ISBN
+  public_users.get('/isbn/:isbn', (req, res) => {
+    const isbn = req.params.isbn;
+  
+    getBookByISBN(isbn)
+      .then((book) => {
+        res.status(200).json(book);
+      })
+      .catch((error) => {
+        res.status(404).json({ message: error });
+      });
+  });
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
